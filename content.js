d1384e213fce99d03654f317aeba3cb3c49bf2eb
Rayan-Allali <108684980+Rayan-Allali@users.noreply.github.com>
2023-03-15 23:05:31 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,24 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   let body = document.getElementsByClassName('a3s')[0]
-  // added console.log statement
+  console.log(body.textContent); // added console.log statement
+  let message=0
+  let data=''
   if (request.action == "get_email_body") {
     sendResponse({body: body.textContent});
+    message=0
+  }
+  else if (request.action == "reply") {
+    
+    data=request.data// "Hello from popup!"
+    message=1
+  }
+  if(message === 1){
+  console.log("///////////////")
+  console.log(data)
+  console.log("///////////////")
+  let button = document.querySelector('.ams.bkH')
+
+  button.click()
+ 
   }
 });
